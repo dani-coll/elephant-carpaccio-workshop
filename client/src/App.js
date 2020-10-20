31979f1f8bc4616f60b4911d0f3e59ce48008ca3
Dani Coll <dani.coll@Danis-MacBook-Pro.local>
2020-10-20 11:15:37 +0200

    Slide 1,2,3,4 & 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const taxes = {
+  UT: 6.85,
+  NV: 8,
+  TX: 6.25,
+  AL: 4,
+  CA: 8.25
+}
+
+const discounts = {
+  1000: 3,
+  5000: 5,
+  7000: 7,
+  10000: 10,
+  50000: 15
+}
+
 function App() {
+
+  function calculate() {
+    const totalItems = document.getElementById('totalItems').value;
+    const price = document.getElementById('price').value;
+
+    const output = document.getElementById('output');
+
+    output.textContent = applyTax(applyDiscount(totalItems * price));
+  }
+
+  function applyDiscount(totalPrice) {
+    let discount;
+    const numbers = Object.keys(discounts)
+    for(let i = 0; i < numbers.length; ++i) {
+      if(totalPrice >= numbers[i]) {
+        discount = discounts[numbers[i]];
+      }
+    }
+
+    if(!discount) {
+      return totalPrice
+    }
+    return totalPrice * (1 - discount/100)
+  }
+
+  function applyTax(totalPrice) {
+    const tax = taxes[document.getElementById('tax').value];
+    return totalPrice * (1 + tax/100)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="totalItems" type="number"></input>
+      <input id="price" type="number"></input>
+      <input id="tax" placeholder="State(XX)" type="text"></input>
+      <button onClick={calculate}>CALCULATE</button>
+      <p id="output"></p>
     </div>
   );
+
+
+
 }
 
 export default App;
